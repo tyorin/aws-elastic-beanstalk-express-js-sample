e06d6ad43de517bd85d6f299b3f8af5f1e8bc0e2
中山 彰 <rincopin@gmail.com>
2021-06-07 13:51:53 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('こんにちは 世界！'));
+app.get('/', (req, res) => res.send('Hallo Welt!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
